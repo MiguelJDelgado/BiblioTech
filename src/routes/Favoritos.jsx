8890b2f04c2dac79b165896be779530f041ad84d
MiguelJDelgado <migueljd193@gmail.com>
2025-04-18 18:17:48 -0300

    Rota, serviços e estilização de Favoritos

--- src/routes/Favoritos.jsx
+++ src/routes/Favoritos.jsx
@@ -1,20 +1,77 @@
-import Header from '../components/Header'
+import { useEffect, useState } from 'react';
 import styled from 'styled-components'
-import Search from '../components/Search';
-import UltimosLancamentos from '../components/UltimosLancamentos';
+import { getFavoritos } from '../services/favoritos';
+import livro from '../assets/livro.png'
 
 const AppContainer = styled.div`
     width: 100vw;
     min-height: 100vh; /* Garante que o fundo cresça conforme o conteúdo */
-    background-image: linear-gradient(90deg, #002F52 35%, #326589);
+    background-image: linear-gradient(90deg, #002F52 35%, #326589);t
+`
+
+const ResultadoContainer = styled.div`
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: center;
+`
+
+const Resultado = styled.div`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   margin: 20px 0;
+   cursor: pointer;
+   text-align: center;
+   padding: 0 100px;
+   p {
+       width: 200px;
+       color: #FFF;
+   }
+   img {
+       width: 100px;
+   }
+   &:hover {
+       border: 1px solid white;
+   }
+`
+
+const Titulo = styled.h2`
+   color: #FFF;
+   font-size: 36px;
+   text-align: center;
+   width: 100%;
+   padding-top: 35px
 `
 
 function Favoritos() {
+  const [favoritos, setFavoritos] = useState([])
+
+  async function fetchFavoritos() {
+    const favoritosDaAPI = await getFavoritos()
+    setFavoritos(favoritosDaAPI)
+  }
+
+  useEffect(() => {
+    fetchFavoritos()
+  }, [])
+
   return (
-      <AppContainer>
-        <Search />
-      </AppContainer>
-  );
+    <AppContainer>
+      <div>
+        <Titulo>Aqui estão seus livros favoritos:</Titulo>
+        <ResultadoContainer>
+          {
+            favoritos.length !== 0 ? favoritos.map(favorito => (
+              <Resultado>
+                <p>{favorito.nome}</p>
+                <img src={livro}/>
+              </Resultado>
+            )) : null
+          }
+        </ResultadoContainer>
+      </div>
+    </AppContainer>
+);
 }
 
 export default Favoritos
